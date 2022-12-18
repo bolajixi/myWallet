1cd34885840ef876640daaa084baf3af9802494c
Mobolaji Olawale <mobolaji.olawale@flutterwavego.com>
2022-12-18 21:20:48 +0100

    Extend Flutterwave service

--- services/flutterwave.js
+++ services/flutterwave.js
@@ -1,47 +1,65 @@
 const Flutterwave = require('flutterwave-node-v3');
-const flw = new Flutterwave(process.env.FLUTTERWAVE_PUBLIC_TEST_KEY, process.env.FLUTTERWAVE_SECRET_KEY);
+const flw = new Flutterwave(process.env.FLUTTERWAVE_PUBLIC_KEY, process.env.FLUTTERWAVE_SECRET_KEY);
 
-module.export = {
+const Transaction = require('../models/transaction');
+const ErrorResponse = require('../utils/errorResponse')
+
+module.exports = {
     /*****  Collections  *****/
     chargeCard: async (payload) => {
         // Pay directly to someones account
         try {
             const response = await flw.Charge.card(payload)
             let reCallCharge
-            console.log(response)
-
-            // Authorizing transactions
-
-            if (response.meta.authorization.mode === 'pin') {
-                let authPayload = payload
-                authPayload.authorization = {
-                    "mode": "pin",
-                    "fields": [ "pin" ],
-                    "pin": 3310
-                }
-                reCallCharge = await flw.Charge.card(authPayload)
+            
+            if(response.status == 'error'){
+                throw new Error(response.message)
             }
 
-            console.log(response)
+            // Authorizing transactions
+            if (response.meta.authorization.mode === 'pin') {
+                let chargePayload = payload
+                chargePayload.authorization = {
+                    "mode": "pin",
+                    "fields": [ "pin" ],
+                    "pin": payload.pin
+                }
+                reCallCharge = await flw.Charge.card(chargePayload)
+            }
+
+            // Store reCallCharge in session
             return reCallCharge
         } catch (error) {
-            console.log(error)
+            throw new ErrorResponse(error.message,400)
         }
     },
 
     authorizeCardPayment: async (reCallCharge) => {
         // Add the OTP to authorize the transaction
-        const callValidate = await flw.Charge.validate({
-            otp: "12345",
+        let status, transaction;
+
+        const response = await flw.Charge.validate({
+            otp: req.body.otp,
             flw_ref: reCallCharge.data.flw_ref
         })
-        console.log(callValidate)
-        return callValidate
+
+        if (response.data.status === 'successful' || response.data.status === 'pending') {
+            // Verify the payment
+            const transactionId = response.data.id;
+            transaction = flw.Transaction.verify({ id: transactionId });
+
+            status = transaction.data.status;
+
+            transactions = await Transaction.createOrUpdate({ transaction: transaction}); // Update the transaction
+            return transactions;
+        }
+        
+        transaction.status = 'failed';
+        const transactions = await Transaction.createOrUpdate(transaction);
+        return transactions;
     },
 
-    credit: () => {
-        // generate link to credit my own wallet
-    },
+    /*****  Transfers  *****/
     transfers: () => {
         // transfer directly to another customer using this app
     },
